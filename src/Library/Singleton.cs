d90816dd1b679564669f285cc97575b7d38dc612
giovannamandirola <69222964+giovannamandirola@users.noreply.github.com>
2020-09-27 20:50:47 -0300

    Singleton genérico

---
+++ src/Library/Singleton.cs
@@ -0,0 +1,24 @@
+
+using System;
+
+public class Singleton<T> where T: new()
+{
+    private Singleton()
+        {
+        }
+
+        private static T instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new T();
+                }
+
+                return instance;
+            }
+        }
+}
